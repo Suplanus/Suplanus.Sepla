966fa47441c23b0ba7c6003179c29762392c4747
Johann Weiher <info@suplanus.de>
2016-02-04 11:34:40 +0100

    Helper added
    Get project or objects of project

---
+++ Suplanus.Sepla/Helper/Objects.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Eplan.EplApi.DataModel;
+
+namespace Suplanus.Sepla.Helper
+{
+	public class Objects
+	{
+		public static List<T> GetAllObjects<T>(Eplan.EplApi.DataModel.Project project)
+		{
+			List<T> objects = new List<T>();
+			foreach (Page page in project.Pages)
+			{
+				objects.AddRange(page.AllPlacements.OfType<T>());
+			}
+			return objects;
+		}
+
+	}
+}
---
+++ Suplanus.Sepla/Helper/Project.cs
@@ -0,0 +1,15 @@
+﻿using Eplan.EplApi.HEServices;
+
+namespace Suplanus.Sepla.Helper
+{
+	public class Project
+	{
+		public static Eplan.EplApi.DataModel.Project GetCurrentProject()
+		{
+			SelectionSet selSet = new SelectionSet();
+			selSet.LockProjectByDefault = true;
+			selSet.LockSelectionByDefault = true;
+			return selSet.GetCurrentProject(true);
+		}
+	}
+}
