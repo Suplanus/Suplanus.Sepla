8db4085e11fbbbad62b61e0a68a21556fd99d6d1
Johann Weiher <info@suplanus.de>
2016-02-10 09:33:48 +0100

    Generate added

---
+++ Suplanus.Example.Generate/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Suplanus.Sepla.Application;
+using Suplanus.Sepla.Helper;
+
+namespace Suplanus.Example.Generate
+{
+	public class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			// Demo data
+			Console.WriteLine("Get Demodata...");
+			string projectLinkPath = @"\\Mac\Home\Desktop\Test.elk";
+			string projectTemplatePath = @"\\Mac\Home\Documents\GitHub\ibKastl.MechatronicsConfigurator\DemoData\Templates\IEC_bas001.zw9";
+
+			List<string> pageMacros = new List<string>
+			{
+				@"\\Mac\Home\Documents\GitHub\ibKastl.MechatronicsConfigurator\DemoData\Macros\PageMacro_Axis-X.emp",
+				@"\\Mac\Home\Documents\GitHub\ibKastl.MechatronicsConfigurator\DemoData\Macros\PageMacro_Supply.emp",
+				// check overwrite
+				@"\\Mac\Home\Documents\GitHub\ibKastl.MechatronicsConfigurator\DemoData\Macros\PageMacro_Axis-X.emp",
+				@"\\Mac\Home\Documents\GitHub\ibKastl.MechatronicsConfigurator\DemoData\Macros\PageMacro_Supply.emp",
+			};
+
+			// Start EPLAN
+			Console.WriteLine("Start EPLAN...");
+			var eplanOffline = new EplanOffline();
+			eplanOffline.StartWithoutGui();
+			if (!eplanOffline.IsRunning)
+			{
+				throw new Exception("EPLAN not running");
+			}
+
+			// Generate
+			Console.WriteLine("Generate...");
+			ProjectUtility.Generate(projectLinkPath, projectTemplatePath, pageMacros);
+
+			// Close EPLAN
+			Console.WriteLine("Closing EPLAN...");
+			eplanOffline.Close();
+
+			// Finish
+			Console.WriteLine("Finished :^) ");
+			Console.ReadKey();
+		}
+	}
+}
--- Suplanus.Sepla/Helper/Project.cs
+++ Suplanus.Sepla/Helper/Project.cs
@@ -1,4 +1,7 @@
-﻿using Eplan.EplApi.DataModel;
+﻿using System.Collections.Generic;
+using System.IO;
+using Eplan.EplApi.DataModel;
+using Eplan.EplApi.DataModel.MasterData;
 using Eplan.EplApi.HEServices;
 
 namespace Suplanus.Sepla.Helper
@@ -13,11 +16,62 @@ namespace Suplanus.Sepla.Helper
 			return selectionSet.GetCurrentProject(true);
 		}
 
-		public static Project Create(string projectLinkFilePath, string projectTemplateFilePath)
+		public static Project Create(string projectLinkFilePath, string projectTemplateFilePath, bool overwrite)
 		{
 			ProjectManager projectManager = new ProjectManager();
-			var project = projectManager.CreateProject(projectLinkFilePath, projectTemplateFilePath);
+			Project project = null;
+
+			using (new LockingStep()) // needed
+			{
+				// Exists
+				if (File.Exists(projectLinkFilePath) && overwrite == false)
+				{
+					project = projectManager.OpenProject(projectLinkFilePath);
+				}
+
+				// New
+				if (!File.Exists(projectLinkFilePath) || overwrite == true)
+				{
+
+					project = projectManager.CreateProject(projectLinkFilePath, projectTemplateFilePath);
+				}
+			}
+
 			return project;
 		}
+
+		public static void Generate(string projectLinkFilePath, string projectTemplateFilePath, List<string> pageMacros)
+		{
+			var project = Create(projectLinkFilePath, projectTemplateFilePath, false);
+
+			Insert insert = new Insert();
+			foreach (var pageMacroFile in pageMacros)
+			{
+				// Load pages from macro
+				PageMacro pageMacroOriginal = new PageMacro();
+				PageMacro pageMacroNew = new PageMacro();
+				pageMacroOriginal.Open(pageMacroFile, project);
+
+				foreach (var page in pageMacroOriginal.Pages)
+				{
+					PagePropertyList ppl = page.NameParts;
+					ppl[Properties.Page.DESIGNATION_PLANT] = "TEST";
+					ppl = page.NameParts;
+					NameService nameServ = new NameService(page);
+					nameServ.EvaluateAndSetAllNames();
+				}
+				
+
+				var tempMacro = Path.Combine(Path.GetTempPath(), "Suplanus.Sepla.Generator.TempMacro.emp");
+				pageMacroOriginal.Create(tempMacro, pageMacroOriginal.Pages, pageMacroOriginal.Description);
+				pageMacroOriginal.Open(tempMacro, project);
+
+				// Insert in Project
+				StorableObject[] insertedPages = insert.PageMacro(
+					pageMacroOriginal, project, null, PageMacro.Enums.NumerationMode.Number);
+
+			}
+		}
+
 	}
 }
