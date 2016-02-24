bf60117fac65e693cb831ae699e4fff1d357919d
Johann Weiher <info@suplanus.de>
2016-02-24 14:29:12 +0100

    Multilanguage Helper added

---
+++ Suplanus.Sepla/Helper/MultiLanguage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Eplan.EplApi.Base;
+
+namespace Suplanus.Sepla.Helper
+{
+	public class MultiLanguage
+	{
+
+		public static ISOCode.Language GuiLanguage
+		{
+			get
+			{
+				return new Languages().GuiLanguage.GetNumber();
+			}
+		}
+
+	}
+}
