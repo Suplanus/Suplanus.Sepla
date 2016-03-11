54e15d29892735f96baebf50038b61860020cf9b
Johann Weiher <info@suplanus.de>
2016-03-11 10:36:24 +0100

    Helper: MultiLanguage DisplayLanguages added

--- Suplanus.Sepla/Helper/MultiLanguage.cs
+++ Suplanus.Sepla/Helper/MultiLanguage.cs
@@ -1,19 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Specialized;
 using System.Linq;
-using System.Text;
 using Eplan.EplApi.Base;
+using Eplan.EplApi.DataModel;
 
 namespace Suplanus.Sepla.Helper
 {
 	public class MultiLanguage
 	{
-
 		public static ISOCode.Language GuiLanguage
 		{
 			get
 			{
 				return new Languages().GuiLanguage.GetNumber();
+			}
+		}
+
+		public static StringCollection DisplayLanguages()
+		{
+			using (new LockingStep())
+			{
+				var project = ProjectUtility.GetCurrentProject();
+				ProjectSettings projectSettings = new ProjectSettings(project);
+				var displayLanguagesString = projectSettings.GetStringSetting("TRANSLATEGUI.DISPLAYED_LANGUAGES", 0);
+
+				var languages = new StringCollection();
+				var displaylanguages = displayLanguagesString.Split(';')
+					.Where(s => !string.IsNullOrWhiteSpace(s)).Distinct().ToArray(); // remove empty
+				languages.AddRange(displaylanguages);
+
+				return languages;
 			}
 		}
 
