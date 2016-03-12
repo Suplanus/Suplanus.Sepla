67265b1b769fa09c5db28d2fb5b42f4639ee4414
Johann Weiher <info@suplanus.de>
2016-03-12 01:02:46 +0100

    Helper: Masterdata added

---
+++ Suplanus.Sepla/Helper/MasterdataUtility.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Specialized;
+using Eplan.EplApi.DataModel;
+using Eplan.EplApi.HEServices;
+
+namespace Suplanus.Sepla.Helper
+{
+	public class MasterdataUtility
+	{
+		bool IsFormInProject(Project project, string formName)
+		{
+			return new Masterdata().get_ProjectEntries(project).Contains(formName);
+		}
+
+		bool IsFormInSystem(string formName)
+		{
+			return new Masterdata().SystemEntries.Contains(formName);
+		}
+
+		bool AddFormToProject(Project project, string formName)
+		{
+			Masterdata masterdata = new Masterdata();
+			StringCollection newForms = new StringCollection();
+			StringCollection projectForms = masterdata.get_ProjectEntries(project);
+
+			if (!IsFormInSystem(formName))
+			{
+				return false;
+			}
+
+			if (!projectForms.Contains(formName))
+			{				
+				newForms.Add(formName);
+				masterdata.AddToProjectEx(project, newForms);
+			}
+			return true;
+		}
+	}
+}
