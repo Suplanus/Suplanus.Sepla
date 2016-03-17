b822aa6675f7f28aff768d2bf7e70d9125a317db
Johann Weiher <info@suplanus.de>
2016-03-17 15:07:16 +0100

    InterruptionPointUtlity added

---
+++ Suplanus.Sepla/Helper/InterruptionPointUtility.cs
@@ -0,0 +1,26 @@
+﻿using Eplan.EplApi.DataModel;
+using Eplan.EplApi.HEServices;
+
+namespace Suplanus.Sepla.Helper
+{
+	public class InterruptionPointUtility
+	{
+		public static void RenameAll(InterruptionPoint interruptionPoint, FunctionBasePropertyList functionBasePropertyList)
+		{
+			// Get all Interruptionpoints
+			DMObjectsFinder objectsFinder = new DMObjectsFinder(interruptionPoint.Project);
+			InterruptionPointsFilter interruptionPointsFilter = new InterruptionPointsFilter();
+			interruptionPointsFilter.Name = interruptionPoint.Name;
+			InterruptionPoint[] interruptionPoints = objectsFinder.GetInterruptionPoints(interruptionPointsFilter);
+
+			// Device tag
+			NameService nameService = new NameService(interruptionPoint.Page);
+			foreach (InterruptionPoint ip in interruptionPoints)
+			{
+				ip.LockObject();
+				ip.NameParts = functionBasePropertyList;
+				nameService.AdjustVisibleName(ip);
+			}
+		}
+	}
+}
