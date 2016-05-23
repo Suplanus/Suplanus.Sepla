db67308f3c32761b5be5dd629b8bcf4e94f6b6fa
Johann Weiher <info@suplanus.de>
2016-05-23 09:26:16 +0200

    OfflineApp: Only starting with references

--- Suplanus.Example.Preview/MainWindow.xaml.cs
+++ Suplanus.Example.Preview/MainWindow.xaml.cs
@@ -8,49 +8,52 @@ using Suplanus.Sepla.Gui;
 
 namespace Suplanus.Example.Preview
 {
-	public partial class MainWindow
-	{
-		// ReSharper disable once AssignNullToNotNullAttribute
-		// DemoData should exist
-		static readonly string DemoData = Path.Combine(
-			Path.GetDirectoryName(Path.GetDirectoryName(
-				Path.GetDirectoryName(Path.GetDirectoryName(AssemblyDirectory)))), "Demodata");
+   public partial class MainWindow
+   {
+      // ReSharper disable once AssignNullToNotNullAttribute
+      // DemoData should exist
+      static readonly string DemoData = Path.Combine(
+         Path.GetDirectoryName(Path.GetDirectoryName(
+            Path.GetDirectoryName(Path.GetDirectoryName(AssemblyDirectory)))), "Demodata");
 
-		readonly string _macroPath = Path.Combine(DemoData, "PageMacro.emp");
-		readonly string _previewProject = Path.Combine(DemoData, "Template.elk");
+      readonly string _macroPath = Path.Combine(DemoData, "PageMacro.emp");
+      readonly string _previewProject = Path.Combine(DemoData, "Template.elk");
 
-		private EplanOffline _eplanOffline;
-		private Sepla.Gui.Preview _preview;
+      private EplanOffline _eplanOffline;
+      private Sepla.Gui.Preview _preview;
 
 
-		public MainWindow()
-		{
-			InitializeComponent();
+      public MainWindow()
+      {
+         InitializeComponent();
+      }
 
-			Init();
-		}
+      public static string AssemblyDirectory
+      {
+         get { return AppDomain.CurrentDomain.BaseDirectory; }
+      }
 
-		public static string AssemblyDirectory
-		{
-			get { return AppDomain.CurrentDomain.BaseDirectory; }
-		}
+      private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+      {
+         Init();
+      }
 
-		private void Init()
-		{
-			// check files	
-			if (!File.Exists(_previewProject))
-			{
-				throw new Exception("Preview project not found:" + Environment.NewLine + _previewProject);
-			}
-			if (!File.Exists(_macroPath))
-			{
-				throw new Exception("Macro not found:" + Environment.NewLine + _macroPath);
-			}
+      private void Init()
+      {
+         // check files	
+         if (!File.Exists(_previewProject))
+         {
+            throw new Exception("Preview project not found:" + Environment.NewLine + _previewProject);
+         }
+         if (!File.Exists(_macroPath))
+         {
+            throw new Exception("Macro not found:" + Environment.NewLine + _macroPath);
+         }
 
          // start eplan			
          _eplanOffline = new EplanOffline();
          //_eplanOffline.StartWpf(this);
-         _eplanOffline.StartWpf(this, @"C:\Program Files\EPLAN\Platform\2.5.4\Bin");
+         _eplanOffline.StartWpf(this, @"C:\Program Files\EPLAN\Electric P8\2.5.4\Bin");
          if (!_eplanOffline.IsRunning)
          {
             throw new Exception("EPLAN not running");
@@ -59,15 +62,17 @@ namespace Suplanus.Example.Preview
          // setup preview
          _preview = new Sepla.Gui.Preview(previewBorder, _previewProject);
 
-			// display
-			_preview.Display(_macroPath, PreviewType.PageMacro);
-		}
+         // display
+         _preview.Display(_macroPath, PreviewType.PageMacro);
+      }
 
 
 
-		private void MainWindow_OnClosing(object sender, CancelEventArgs e)
-		{
-			_eplanOffline.Close();
-		}
-	}
+      private void MainWindow_OnClosing(object sender, CancelEventArgs e)
+      {
+         _eplanOffline.Close();
+      }
+
+
+   }
 }
