7e1acab6b87235b2a7119895c1c193d9063bb520
Johann Weiher <info@suplanus.de>
2016-10-20 12:50:12 +0200

    LocationUtility added

---
+++ Suplanus.Sepla/Helper/LocationUtility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Eplan.EplApi.DataModel;
+
+namespace Suplanus.Sepla.Helper
+{
+   /// <summary>
+   /// Helper for Locations in EPLAN
+   /// </summary>
+   public static class LocationUtility
+   {
+      /// <summary>
+      /// Orders all location ascending
+      /// </summary>
+      /// <param name="project"></param>
+      public static void OrderLocation(Project project)
+      {
+         var hierachies = Enum.GetValues(typeof(Project.Hierarchy)); // Get all types
+         foreach (Project.Hierarchy hierachy in hierachies)
+         {
+            string[] locations = project.GetLocations(hierachy);
+            if (locations != null) // could be null
+            {
+               locations = locations.OrderBy(o => o).ToArray(); // Order
+               project.SetSortedLocations(hierachy, locations);
+            }
+         }
+      }
+   }
+}
