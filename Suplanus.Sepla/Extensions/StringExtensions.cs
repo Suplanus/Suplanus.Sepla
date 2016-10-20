9853254b3bf262b72cc255a58f741e36d8626f5d
Johann Weiher <info@suplanus.de>
2016-10-20 12:49:59 +0200

    Extensions added

---
+++ Suplanus.Sepla/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Suplanus.Sepla.Extensions
+{
+   public static class StringExtensions
+   {
+      public static bool IsNullOrEmpty(this string s)
+      {
+         return string.IsNullOrEmpty(s);
+      }
+   }
+}
