eb644d5d273450e7fdc84ca0c1829ea2813b29e1
Johann Weiher <info@suplanus.de>
2017-07-31 11:19:04 +0200

    PartUtility added

---
+++ Suplanus.Sepla/Helper/PartUtility.cs
@@ -0,0 +1,20 @@
+﻿using Eplan.EplApi.MasterData;
+using Eplan.EplApi.System;
+
+namespace Suplanus.Sepla.Helper
+{
+   public class PartUtility
+   {
+      public static MDPart GetPart()
+      {
+         EplApplication eplanApplication = new EplApplication();
+         MDPartsManagement partsManagement = new MDPartsManagement();
+         string partnNumber = string.Empty;
+         string partVariant = string.Empty;
+         eplanApplication.ShowPartSelectionDialog(ref partnNumber, ref partVariant);
+         MDPartsDatabase partsDatabase = partsManagement.OpenDatabase();
+         MDPart part = partsDatabase.GetPart(partnNumber, partVariant);
+         return part;
+      }
+   }
+}
