d0ab7648b510059c80c90e6348c6855e538f9b63
Suplanus <info@suplanus.de>
2017-11-15 16:19:46 +0100

    PageUtility Macro representationType

--- Suplanus.Sepla/Helper/PageUtility.cs
+++ Suplanus.Sepla/Helper/PageUtility.cs
@@ -1,5 +1,6 @@
 ﻿using Eplan.EplApi.ApplicationFramework;
 using Eplan.EplApi.DataModel;
+using Eplan.EplApi.DataModel.MasterData;
 using Eplan.EplApi.HEServices;
 
 namespace Suplanus.Sepla.Helper
@@ -31,6 +32,23 @@ namespace Suplanus.Sepla.Helper
             new CommandLineInterpreter().Execute("XGedSelectPageAction"); // Select page
             new CommandLineInterpreter().Execute("XEsSyncPDDsAction"); // Sync selection
             new CommandLineInterpreter().Execute("XGedEscapeAction"); // Escape (page selection)
+        }
+
+        public static WindowMacro.Enums.RepresentationType GetMacroRepresentationType(Page page)
+        {
+            switch (page.PageType)
+            {
+                case DocumentTypeManager.DocumentType.CircuitFluid: return WindowMacro.Enums.RepresentationType.Fluid_MultiLine;
+                case DocumentTypeManager.DocumentType.Circuit: return WindowMacro.Enums.RepresentationType.MultiLine;
+                case DocumentTypeManager.DocumentType.CircuitSingleLine: return WindowMacro.Enums.RepresentationType.SingleLine;
+                case DocumentTypeManager.DocumentType.Overview: return WindowMacro.Enums.RepresentationType.Overview;
+                case DocumentTypeManager.DocumentType.Graphics: return WindowMacro.Enums.RepresentationType.Graphics;
+                case DocumentTypeManager.DocumentType.ProcessAndInstrumentationDiagram: return WindowMacro.Enums.RepresentationType.PI_FlowChart;
+                case DocumentTypeManager.DocumentType.PanelLayout: return WindowMacro.Enums.RepresentationType.ArticlePlacement;
+                case DocumentTypeManager.DocumentType.Topology: return WindowMacro.Enums.RepresentationType.Cabling;
+                case DocumentTypeManager.DocumentType.Planning: return WindowMacro.Enums.RepresentationType.Planning;
+                default: return WindowMacro.Enums.RepresentationType.Default;
+            }
         }
     }
 }
