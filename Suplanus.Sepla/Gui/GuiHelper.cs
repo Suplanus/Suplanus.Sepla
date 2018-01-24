dc17c3d1f7b9a3b8f760e3a86ebea127e6139c62
Suplanus <info@suplanus.de>
2018-01-24 08:41:21 +0100

    GuiHelper added

---
+++ Suplanus.Sepla/Gui/GuiHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace Suplanus.Sepla.Gui
+{
+  public class GuiHelper
+  {
+    public static void SetEplanAsOwner(Window window)
+    {
+      WindowInteropHelper windowInteropHelper = new WindowInteropHelper(window);
+      windowInteropHelper.Owner = Process.GetCurrentProcess().MainWindowHandle;
+    }
+  }
+}
