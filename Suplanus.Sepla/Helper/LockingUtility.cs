476a41398d8c2643eeaad1be3d7892ec9127ed9b
Suplanus <info@suplanus.de>
2018-01-24 08:40:39 +0100

    LockingUtility added

---
+++ Suplanus.Sepla/Helper/LockingUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Eplan.EplApi.DataModel;
+
+namespace Suplanus.Sepla.Helper
+{
+  public class LockingUtility
+  {
+    public class SeplaLockingVector : IDisposable
+    {
+      private readonly int _manualLockStateId;
+      private readonly LockingVector _lockingVector;
+
+      public SeplaLockingVector()
+      {
+        _lockingVector = new LockingVector();
+        _manualLockStateId = _lockingVector.PauseManualLock();
+      }
+
+
+      public void Dispose()
+      {
+        _lockingVector.ResumeManualLock(_manualLockStateId);
+        _lockingVector.Dispose();
+      }
+    }
+  }
+}
