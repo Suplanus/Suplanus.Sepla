c880d536722f6a2e7da0648f9f17e3613f15200e
Suplanus <info@suplanus.de>
2018-01-24 08:41:07 +0100

    SymbolUtility locking

--- Suplanus.Sepla/Helper/SymbolUtility.cs
+++ Suplanus.Sepla/Helper/SymbolUtility.cs
@@ -4,39 +4,42 @@ using Eplan.EplApi.DataModel.MasterData;
 
 namespace Suplanus.Sepla.Helper
 {
+  /// <summary>
+  /// Helper class for symbols
+  /// </summary>
+  public class SymbolUtility
+  {
     /// <summary>
-    /// Helper class for symbols
+    /// Insert a symbol on a given page
     /// </summary>
-    public class SymbolUtility
+    /// <param name="page">Page where the symbol be insert</param>
+    /// <param name="symbolLibraryName">Symbol library name</param>
+    /// <param name="symbolName">Symbol name</param>
+    /// <param name="symbolvariant">Symbol variant</param>
+    public void Insert(Page page, string symbolLibraryName, string symbolName, int symbolvariant)
     {
-        /// <summary>
-        /// Insert a symbol on a given page
-        /// </summary>
-        /// <param name="page">Page where the symbol be insert</param>
-        /// <param name="symbolLibraryName">Symbol library name</param>
-        /// <param name="symbolName">Symbol name</param>
-        /// <param name="symbolvariant">Symbol variant</param>
-        public void Insert(Page page, string symbolLibraryName, string symbolName, int symbolvariant)
-        {
-            page.SmartLock();
+      page.SmartLock();
 
-            SymbolLibrary symbolLibrary = new SymbolLibrary(page.Project, symbolLibraryName);
-            Symbol symbol = new Symbol(symbolLibrary, symbolName);
+      SymbolLibrary symbolLibrary = new SymbolLibrary(page.Project, symbolLibraryName);
+      Symbol symbol = new Symbol(symbolLibrary, symbolName);
 
-            Function function = new Function();
-            function.Create(page.Project, symbol.Variants[symbolvariant]);
-            function.Location = new PointD(200, 150);
-            page.InsertSubPlacement(function);
-            function.Dispose();
-        }
-
-        public static Function GetFunction(Page page, string symbolLibraryName, string symbolName, int symbolvariant)
-        {
-            SymbolLibrary symbolLibrary = new SymbolLibrary(page.Project, symbolLibraryName);
-            Symbol symbol = new Symbol(symbolLibrary, symbolName);
-            Function function = new Function();
-            function.Create(page.Project, symbol.Variants[symbolvariant]);
-            return function;
-        }
+      Function function = new Function();
+      function.Create(page.Project, symbol.Variants[symbolvariant]);
+      function.Location = new PointD(200, 150);
+      page.InsertSubPlacement(function);
+      function.Dispose();
     }
+
+    public static Function GetFunction(Page page, string symbolLibraryName, string symbolName, int symbolvariant)
+    {
+      page.SmartLock();
+
+      SymbolLibrary symbolLibrary = new SymbolLibrary(page.Project, symbolLibraryName);
+      Symbol symbol = new Symbol(symbolLibrary, symbolName);
+      Function function = new Function();
+
+      function.Create(page.Project, symbol.Variants[symbolvariant]);
+      return function;
+    }
+  }
 }
