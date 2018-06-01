404fd785f3815779d1938e74b6741108437f6fa1
Suplanus <info@suplanus.de>
2018-06-01 14:19:07 +0200

    EplanDispatcher added

---
+++ Suplanus.Sepla/Application/EplanDispatcher.cs
@@ -0,0 +1,17 @@
+﻿using Eplan.EplApi.Base.Internal;
+using Action = System.Action;
+
+namespace Suplanus.Sepla.Application
+{
+  public class EplanDispatcher
+  {
+    public static void ExecuteInMainThread(Action action)
+    {
+      new EplanMainThreadDispatcher().ExecuteInMainThreadSync(o =>
+      {
+        action.Invoke();
+        return null;
+      }, null);
+    }
+  }
+}
