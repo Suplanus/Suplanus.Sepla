d94cccf746862a3c52a001ec0be2cd1a39a4909b
Suplanus <info@suplanus.de>
2018-08-21 07:32:21 +0200

    Refactor

--- Suplanus.Sepla/Properties/AssemblyInfo.cs
+++ Suplanus.Sepla/Properties/AssemblyInfo.cs
@@ -9,35 +9,16 @@ using Eplan.EplApi.Starter;
 [assembly: AssemblyDelaySign(true)]
 #endif
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Suplanus.Sepla")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Suplanus.Sepla")]
-[assembly: AssemblyCopyright("Copyright ?? Suplanus 2017")]
+[assembly: AssemblyCopyright("Copyright ? Suplanus 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a3ac82b1-6ea9-4127-80c8-0e09c49b1cc0")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
