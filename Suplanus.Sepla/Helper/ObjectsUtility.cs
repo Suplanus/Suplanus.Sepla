1fdc92d55c351abd551186d8aaf354d1cdd3b57e
Suplanus <info@suplanus.de>
2018-10-01 15:57:42 +0200

    Refactor

--- Suplanus.Sepla/Helper/ObjectsUtility.cs
+++ Suplanus.Sepla/Helper/ObjectsUtility.cs
@@ -5,51 +5,51 @@ using Eplan.EplApi.HEServices;
 
 namespace Suplanus.Sepla.Helper
 {
-	/// <summary>
-	/// Objects helper
-	/// </summary>
-	public class ObjectsUtility
-	{
-      /// <summary>
-      /// Returns all objects of given type
-      /// </summary>
-      /// <typeparam name="T">EPLAN object type</typeparam>
-      /// <param name="project">EPLAN Object</param>
-      /// <returns>List of objects</returns>
-public static List<T> GetAllObjectsOfType<T>(Project project)
-{
+  /// <summary>
+  /// Objects helper
+  /// </summary>
+  public class ObjectsUtility
+  {
+    /// <summary>
+    /// Returns all objects of given type
+    /// </summary>
+    /// <typeparam name="T">EPLAN object type</typeparam>
+    /// <param name="project">EPLAN Object</param>
+    /// <returns>List of objects</returns>
+    public static List<T> GetAllObjectsOfType<T>(Project project)
+    {
       DMObjectsFinder dmObjectsFinder = new DMObjectsFinder(project);
-	return dmObjectsFinder.GetPlacements(null).OfType<T>().ToList();       
-}
+      return dmObjectsFinder.GetPlacements(null).OfType<T>().ToList();
+    }
 
-      /// <summary>
-      /// Returns all objects of a EPLAN project
-      /// </summary>
-      /// <param name="project">EPLAN project</param>
-      /// <returns>List of Placements</returns>
-		public static List<Placement> GetAllObjects(Project project)
-		{
-			DMObjectsFinder dmObjectsFinder = new DMObjectsFinder(project);
-			return dmObjectsFinder.GetPlacements(null).ToList();
-		}
+    /// <summary>
+    /// Returns all objects of a EPLAN project
+    /// </summary>
+    /// <param name="project">EPLAN project</param>
+    /// <returns>List of Placements</returns>
+    public static List<Placement> GetAllObjects(Project project)
+    {
+      DMObjectsFinder dmObjectsFinder = new DMObjectsFinder(project);
+      return dmObjectsFinder.GetPlacements(null).ToList();
+    }
 
-      /// <summary>
-      /// Returns the selected objects in navigator or in GED
-      /// </summary>
-      /// <returns></returns>
-		public static List<StorableObject> GetSelectedStorableObjects()
-		{
-			SelectionSet selectionSet = new SelectionSet();
+    /// <summary>
+    /// Returns the selected objects in navigator or in GED
+    /// </summary>
+    /// <returns></returns>
+    public static List<StorableObject> GetSelectedStorableObjects()
+    {
+      SelectionSet selectionSet = new SelectionSet();
 
-			// navigators
-			List<StorableObject> storableObjects = selectionSet.SelectionRecursive.ToList();
+      // navigators
+      List<StorableObject> storableObjects = selectionSet.SelectionRecursive.ToList();
 
-			// GED
-			if (!storableObjects.Any())
-			{
-				storableObjects = selectionSet.Selection.ToList();
-			}
-			return storableObjects;
-		}
-	}
+      // GED
+      if (!storableObjects.Any())
+      {
+        storableObjects = selectionSet.Selection.ToList();
+      }
+      return storableObjects;
+    }
+  }
 }
