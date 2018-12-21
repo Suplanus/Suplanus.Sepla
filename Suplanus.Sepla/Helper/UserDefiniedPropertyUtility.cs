8a4c1cfd2ef48135934c760ef0216baeb868dc99
Suplanus <info@suplanus.de>
2018-12-21 10:16:26 +0100

    UserDefiniedProperty refactor

--- Suplanus.Sepla/Helper/UserDefiniedPropertyUtility.cs
+++ Suplanus.Sepla/Helper/UserDefiniedPropertyUtility.cs
@@ -10,6 +10,39 @@ namespace Suplanus.Sepla.Helper
   /// </summary>
   public class UserDefiniedPropertyUtility
   {
+    /// <summary>
+    /// Creates property if it not exist
+    /// </summary>
+    /// <param name="project">EPLAN project</param>
+    /// <param name="identName">Identifying name</param>
+    /// <param name="userDefType">Type of property</param>
+    /// <returns>PropertyValue</returns>
+    public static PropertyValue CreatePropertyIfNotExist(Project project, string identName,
+      UserDefinedPropertyDefinition.Enums.ClientType userDefType)
+    {
+      UserDefinedPropertyDefinition userDefProp = project.UserDefinedPropertyDefinitions
+        .FirstOrDefault(u => u.IdentifyingName.Equals(identName));
+      if (userDefProp == null)
+      {
+        UserDefinedPropertyDefinition.Create(project, identName,
+          userDefType);
+      }
+      return null;
+    }
+
+    /// <summary>
+    /// Creates property if it not exist
+    /// </summary>
+    /// <param name="project">EPLAN project</param>
+    /// <param name="identName">Identifying name</param>
+    /// <returns>PropertyValue</returns>
+    public static bool CheckIfPropertyExists(Project project, string identName)
+    {
+      UserDefinedPropertyDefinition userDefProp = project.UserDefinedPropertyDefinitions
+        .FirstOrDefault(u => u.IdentifyingName.Equals(identName));
+      return userDefProp != null;
+    }
+
     #region Project
 
     /// <summary>
@@ -21,7 +54,7 @@ namespace Suplanus.Sepla.Helper
     public static PropertyValue GetProjectPropertyValueAndCheckIfEmpty(Project project, string identName)
     {
       PropertyValue propertyValue = GetProjectPropertyValue(project, identName);
-      if (propertyValue.IsEmpty)
+      if (propertyValue == null || propertyValue.IsEmpty)
       {
         return null;
       }
@@ -53,6 +86,7 @@ namespace Suplanus.Sepla.Helper
     /// <param name="project">EPLAN project</param>
     /// <param name="identName">Identifying name</param>
     /// <returns>PropertyValue</returns>
+    [Obsolete]
     public static PropertyValue CreateProjectPropertyIfNotExist(Project project, string identName)
     {
       UserDefinedPropertyDefinition userDefProp = project.UserDefinedPropertyDefinitions
@@ -119,6 +153,8 @@ namespace Suplanus.Sepla.Helper
     #endregion
 
     #region Function
+
+
 
     /// <summary>
     /// Returns the PropertyValue if not empty
@@ -210,7 +246,7 @@ namespace Suplanus.Sepla.Helper
     }
     #endregion
 
-
+    #region Page
     /// <summary>
     /// Returns the PropertyValue if not empty
     /// </summary>
@@ -247,6 +283,100 @@ namespace Suplanus.Sepla.Helper
       }
       return null;
     }
+    #endregion
+
+    #region ArticleReference
+    /// <summary>
+    /// Returns the PropertyValue if not empty
+    /// </summary>
+    /// <param name="articleReference">Function</param>
+    /// <param name="identName">Identifing name</param>
+    /// <returns>PropertyValue</returns>
+    public static PropertyValue GetArticleReferencePropertyValueAndCheckIfEmpty(ArticleReference articleReference, string identName)
+    {
+      PropertyValue propertyValue = GetArticleReferencePropertyValue(articleReference, identName);
+      if (propertyValue.IsEmpty)
+      {
+        return null;
+      }
+      return propertyValue;
+    }
+
+    /// <summary>
+    /// Returns the PropertyValue
+    /// </summary>
+    /// <param name="articleReference">Function</param>
+    /// <param name="identName">Identifing name</param>
+    /// <returns>PropertyValue</returns>
+    public static PropertyValue GetArticleReferencePropertyValue(ArticleReference articleReference, string identName)
+    {
+      UserDefinedPropertyDefinition userDefProp = articleReference.Properties.ExistingIds
+         .Select(anyPropertyId => anyPropertyId.Definition)
+         .OfType<UserDefinedPropertyDefinition>()
+         .FirstOrDefault(obj => obj.IdentifyingName.Equals(identName));
+      if (userDefProp != null)
+      {
+        AnyPropertyId anyPropertyId = userDefProp.Id;
+        PropertyValue propertyValue = articleReference.Properties[anyPropertyId];
+        return propertyValue;
+      }
+      return null;
+    }
+
+    /// <summary>
+    /// Sets a value to PropertyValue
+    /// </summary>
+    /// <param name="articleReference">Function</param>
+    /// <param name="identName">Identifing name</param>
+    /// <param name="value">New value</param>
+    public static void SetArticleReferencePropertyValue(ArticleReference articleReference, string identName, object value)
+    {
+      PropertyValue propertyValue = GetArticleReferencePropertyValue(articleReference, identName);
+      if (propertyValue != null)
+      {
+        if (value is bool)
+        {
+          propertyValue.Set((bool)value);
+          return;
+        }
+        if (value is double)
+        {
+          propertyValue.Set((double)value);
+          return;
+        }
+        if (value is MultiLangString)
+        {
+          propertyValue.Set((MultiLangString)value);
+          return;
+        }
+        if (value is PointD)
+        {
+          propertyValue.Set((PointD)value);
+          return;
+        }
+        if (value is int)
+        {
+          propertyValue.Set((int)value);
+          return;
+        }
+        if (value is string)
+        {
+          propertyValue.Set((string)value);
+          return;
+        }
+        if (value is DateTime)
+        {
+          propertyValue.Set((DateTime)value);
+          return;
+        }
+
+        throw new Exception("Type not supported");
+      }
+      throw new Exception("Property not found");
+    }
+
+
+    #endregion
 
   }
 
