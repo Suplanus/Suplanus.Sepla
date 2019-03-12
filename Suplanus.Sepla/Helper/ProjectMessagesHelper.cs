d546485feb62737c886cee4a5cc6cb93ddfd18af
Suplanus <info@suplanus.de>
2019-03-12 15:57:12 +0100

    ProjectMessagesHelper added

---
+++ Suplanus.Sepla/Helper/ProjectMessagesHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Eplan.EplApi.DataModel;
+using Eplan.EplApi.EServices;
+
+namespace Suplanus.Sepla.Helper
+{
+  public class ProjectMessagesHelper
+  {
+    public static List<ProjectMessage> GetProjectMessages(Project project)
+    {
+      List<ProjectMessage> projectMessages = new List<ProjectMessage>();
+      PrjMessagesCollection messagesCollection = new PrjMessagesCollection(project);
+      PrjMessagesEnumerator itPrjMsg = messagesCollection.GetPrjMsgEnumerator();
+      itPrjMsg.MoveNext();
+      do
+      {
+        if (itPrjMsg.Current is ProjectMessage oPrjMsg)
+        {
+          projectMessages.Add(oPrjMsg);
+        }
+      }
+      while (itPrjMsg.MoveNext());
+
+      return projectMessages;
+    }
+  }
+}
