01f10ed2a936d102d190446e21ac0b1bf52f6ec8
Johann Weiher <info@suplanus.de>
2020-10-07 09:56:03 +0200

    EplanOffline: WpfCore example

---
+++ Suplanus.Example.EplanOffline.WpfCore/DoSomething.cs
@@ -0,0 +1,14 @@
+﻿using Eplan.EplApi.DataModel;
+
+namespace Suplanus.Example.EplanOffline.WpfCore
+{
+  internal class DoSomething
+  {
+    public void Foo()
+    {
+      ProjectManager projectManager = new ProjectManager();
+      var count = projectManager.OpenProjects.Length;
+      App.Window.Title = "OpenProjects: " + count;
+    }
+  }
+}
